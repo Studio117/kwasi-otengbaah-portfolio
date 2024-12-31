f572198881ff75c69b65760b1bcc0bc33fab6863
Studio117 <kwasiob@gmail.com>
2024-12-31 01:49:14 -0500

    test push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,7 @@ function Header() {
           <nav className="header__nav">
               <img src={logo} alt="logo" className='header__nav-logo' />
               <MenuIcon className='header__nav-icon' />
+              <MenuIcon className='header__nav-icon' />
           </nav>
     </div>
   )

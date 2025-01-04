69880d6a010b6afc79a81dfab1d94c8f64455601
Studio117 <kwasiob@gmail.com>
2025-01-04 03:48:32 -0500

    added styling and breakpoints to skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,65 +1,72 @@
 import SkillsList from '../SkillsList/SkillsList'
-import checkMark from '../../assets/images/checkmark-dark.svg'
+import checkMarkDark from '../../assets/images/checkmark-light.svg'
+import checkMarkLight from '../../assets/images/checkmark-dark.svg'
 import './Skills.scss'
+import { useTheme } from '../../commonfiles/ThemeContext'
 
 
 import React from 'react'
 
 function Skills() {
+  const { theme } = useTheme();
+
+      const checkMarkIcon = theme === 'light' ? checkMarkDark : checkMarkLight;
+    
+
     return (
         <section id='skills'>
             <div className="skills">
                 <h1 className="sectionTitle">Skills</h1>
-            <div className="skills__category">
-                <h2 className='skills__category-heading'>Frontend</h2>
-                <div className="skills__category-set">
-                    <SkillsList src={checkMark} skill="HTML" />
-                    <SkillsList src={checkMark} skill="CSS" />
-                    <SkillsList src={checkMark} skill="JavaScript" />
-                    <SkillsList src={checkMark} skill="Canva" />
-                    <SkillsList src={checkMark} skill="Bootstrap" />
-                    <SkillsList src={checkMark} skill="Sass" />
-                    <SkillsList src={checkMark} skill="React" />
-                </div>
+                <div className="skills__category">
+                    <h2 className='skills__category-heading'>Frontend</h2>
+                    <div className="skills__category-set">
+                        <SkillsList src={checkMarkIcon} skill="HTML" />
+                        <SkillsList src={checkMarkIcon} skill="CSS" />
+                        <SkillsList src={checkMarkIcon} skill="JavaScript" />
+                        <SkillsList src={checkMarkIcon} skill="Canva" />
+                        <SkillsList src={checkMarkIcon} skill="Bootstrap" />
+                        <SkillsList src={checkMarkIcon} skill="Sass" />
+                        <SkillsList src={checkMarkIcon} skill="React" />
+                    </div>
 
-            </div>
-            <hr />
-            <div className="skills__category">
-                <h2 className='skills__category-heading'>Backend</h2>
-                <div className="skills__category-set">
-                    <SkillsList src={checkMark} skill="Node.js" />
-                    <SkillsList src={checkMark} skill="MySQL" />
-                    <SkillsList src={checkMark} skill="Knex" />
-                    <SkillsList src={checkMark} skill="Express" />
-                    <SkillsList src={checkMark} skill="MongoDB" />
                 </div>
+                <hr />
+                <div className="skills__category">
+                    <h2 className='skills__category-heading'>Backend</h2>
+                    <div className="skills__category-set">
+                        <SkillsList src={checkMarkIcon} skill="Node.js" />
+                        <SkillsList src={checkMarkIcon} skill="MySQL" />
+                        <SkillsList src={checkMarkIcon} skill="Knex" />
+                        <SkillsList src={checkMarkIcon} skill="Express" />
+                        <SkillsList src={checkMarkIcon} skill="MongoDB" />
+                    </div>
 
-            </div>
-            <hr />
-            <div className="skills__category">
-                <h2 className='skills__category-heading'>Design & Collaboration Tools</h2>
-                <div className="skills__category-set">
-                    <SkillsList src={checkMark} skill="Figma" />
-                    <SkillsList src={checkMark} skill="Github" />
-                    <SkillsList src={checkMark} skill="Jira" />
                 </div>
+                <hr />
+                <div className="skills__category">
+                    <h2 className='skills__category-heading'>Design & Collaboration Tools</h2>
+                    <div className="skills__category-set">
+                        <SkillsList src={checkMarkIcon} skill="Figma" />
+                        <SkillsList src={checkMarkIcon} skill="Github" />
+                        <SkillsList src={checkMarkIcon} skill="Jira" />
+                    </div>
 
-            </div>
-            <hr />
-            <div className="skills__category">
-                <h2 className='skills__category-heading'>Content Management & Website Builders</h2>
-                <div className="skills__category-set">
-                    <SkillsList src={checkMark} skill="Wordpress" />
-                    <SkillsList src={checkMark} skill="Webflow" />
-                    <SkillsList src={checkMark} skill="Weebly" />
-                    <SkillsList src={checkMark} skill="Squarespace" />
-                    <SkillsList src={checkMark} skill="Wix" />
                 </div>
+                <hr />
+                <div className="skills__category">
+                    <h2 className='skills__category-heading'>Content Management & Website Builders</h2>
+                    <div className="skills__category-set">
+                        <SkillsList src={checkMarkIcon} skill="Wordpress" />
+                        <SkillsList src={checkMarkIcon} skill="Webflow" />
+                        <SkillsList src={checkMarkIcon} skill="Weebly" />
+                        <SkillsList src={checkMarkIcon} skill="Squarespace" />
+                        <SkillsList src={checkMarkIcon} skill="Wix" />
+                    </div>
 
+                </div>
+                <hr />
             </div>
-            <hr />
-            </div>
-            
+
 
         </section>
     )

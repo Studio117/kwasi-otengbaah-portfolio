0b1ecee279a9cf3042486e99b1d05d6851fc0360
Studio117 <kwasiob@gmail.com>
2025-01-08 22:44:15 -0500

    minor styling and refactoring

--- src/components/SkillsList/SkillsList.jsx
+++ src/components/SkillsList/SkillsList.jsx
@@ -3,12 +3,15 @@ import './SkillsList.scss'
 
 function SkillsList({ src, skill }) {
     return (
-        <section className='skillsList'>
-            <span className='skillsList__item'>
-                     <img src={src} alt="check mark" />
-            <p>{skill}</p>       
-            </span>
-        </section>
+        <div id='skillsList'>
+            <div className="skillsList">
+                <span className='skillsList__item'>
+                    <img src={src} alt="check mark" />
+                    <p>{skill}</p>
+                </span>
+            </div>
+
+        </div>
     )
 }
 

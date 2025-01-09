1dc72dc600d8b2b4691e6a5a991dc9d5dde4b870
Studio117 <kwasiob@gmail.com>
2025-01-09 01:00:11 -0500

    created a homepage component folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Header from './components/Header/Header'
 import HeroSection from './components/HeroSection/HeroSection'
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
 
   return (
     <>
       {/* <Header /> */}
+      <HomePage />
       <HeroSection />
       <Projects />
       <Skills />
